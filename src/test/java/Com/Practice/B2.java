b30aa7bdd698ec107e4f4ad34b942a7888dfc548
admin <admin@DESKTOP-OUT6V2T>
2024-01-25 11:15:08 +0530

    new class is added in practice package

---
+++ src/test/java/Com/Practice/B2.java
@@ -0,0 +1,12 @@
+package Com.Practice;
+
+import org.testng.annotations.Test;
+
+public class B2 {
+	@Test(groups = "Mob")
+	public void Test1()
+	{
+		System.out.println("-- Demo TS1 --");
+	}
+
+}
