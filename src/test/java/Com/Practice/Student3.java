9ef7e942315c295b9e14da1b3837f51aeb49ba88
User <User@DESKTOP-K8P4MC5>
2024-01-25 11:38:57 +0530

    student-3

---
+++ src/test/java/Com/Practice/Student3.java
@@ -0,0 +1,12 @@
+package Com.Practice;
+
+import org.testng.annotations.Test;
+
+public class Student3 {
+	@Test(groups = {"App" , "Mob"})
+	public void Test3()
+	{
+		System.out.println("-- Demo TS3 --");
+	}
+
+}
