69408f0566fc7fccb31939d04b9de51f9d53b4c4
User <User@DESKTOP-K8P4MC5>
2024-01-25 11:10:45 +0530

    added tech-1

---
+++ src/test/java/Com/Practice/Tech1.java
@@ -0,0 +1,14 @@
+package Com.Practice;
+
+import org.testng.annotations.Test;
+
+public class Tech1 {
+	@Test(groups = "App")
+public void CreateAcc()
+{
+	System.out.println("Providng Credentials");
+}
+
+	
+
+}
